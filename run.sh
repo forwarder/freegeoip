ff3c7aaa43664d3203b83fb1d942cc1bc709c038
Eelco Wiersma <contact@pagebakers.nl>
2020-03-17 11:16:50 +0100

    Download custom db

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir -p /tmp/freegeoip
+
+if [ -n "$FREEGEOIP_CUSTOM_UPDATES_URL" ]; then \
+curl -fSLo /tmp/freegeoip/db.gz "${FREEGEOIP_CUSTOM_UPDATES_URL}"; \
+fi
+
+# [ -f "/db.gz" ] && cp /db.gz /tmp/freegeoip/db.gz
+
+exec /go/bin/freegeoip -http :${PORT} -use-x-forwarded-for -public /app/cmd/freegeoip/public -quota-backend map -quota-max 10000000
